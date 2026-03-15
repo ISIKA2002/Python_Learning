4adfd0d7e7f22e34d632ec878d7dc250e503803f
ISIKA2002 <isikapalei05.gmail.com>
2026-03-15 17:29:16 +0530

    conditional

--- python-mastery/02_control_flow/problems/divisiable.py
+++ python-mastery/02_control_flow/problems/divisiable.py
@@ -1,6 +1,6 @@
 #Take a posetive ingeter input and tell it is divisible by 5 or 3 not divisible by 15
 num= int(input("Enter a number:"))
-#Checking if divisible by 15 or not
+#Checking if divisible by 15 or not (use if else)
 if num % 15 ==0:
     print("Number is divisible by 15")
 else:
