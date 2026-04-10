c54605db6b922a95b9d7b074e6a8358159580e7e
ISIKA2002 <isikapalei05.gmail.com>
2026-04-10 10:34:15 +0530

    function

--- python-mastery/05_functions/Problems/palindrome_check.py
+++ python-mastery/05_functions/Problems/palindrome_check.py
@@ -1,7 +1,23 @@
 #Palimdrome= 12321
-n= (input("Enter the number:"))
+n = (input("Enter the number:"))
 
 if n == n[::-1]:
     print("Palindrome")
 else:
     print("Not Palindrome")
+
+#without string or using math
+num=int(input("Enter the number:"))
+
+temp=num
+rev=0
+
+while num>0:
+    digit = num % 10
+    rev = rev * 10 + digit
+    num = num // 10
+
+if temp == rev:
+    print("Palindrome")
+else:
+    print("Not Palindrome")    
